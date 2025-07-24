2b16fed1bf773dc4c1727458d93a97f8433b17bb
beom-jin.kim <beom-jin.kim@samsung.com>
2025-07-24 13:44:35 +0900

    [Step 12] Refactoring to merge BookingScheuler mock

--- RestaurantBooking/test.cpp
+++ RestaurantBooking/test.cpp
@@ -2,8 +2,7 @@
 #include "booking_scheduler.cpp"
 #include "testable_sms_sender.cpp"
 #include "testable_mail_sender.cpp"
-#include "sunday_booking_scheduler.cpp"
-#include "monday_booking_scheduler.cpp"
+#include "testable_booking_scheduler.cpp"
 
 using namespace std;
 using namespace testing;
@@ -24,6 +23,8 @@ public:
 
 	tm NOT_ON_THE_HOUR;
 	tm ON_THE_HOUR;
+	tm SUNDAY_ON_THE_HOUR;
+	tm MONDAY_ON_THE_HOUR;
 	Customer CUSTOMER{ "Fake name", "010-1234-5678" };
 	Customer CUSTOMER_WITH_MAIL{ "Fake name", "010-1234-5678", "test@test.com" };
 	const int UNDER_CAPACITY = 1;
@@ -37,6 +38,9 @@ protected:
 	void SetUp() override {
 		NOT_ON_THE_HOUR = getTime(2021, 3, 26, 9, 5);
 		ON_THE_HOUR = getTime(2021, 3, 26, 9, 0);
+		SUNDAY_ON_THE_HOUR = getTime(2021, 3, 28, 17, 0);
+		MONDAY_ON_THE_HOUR = getTime(2024, 6, 3, 17, 0);
+
 
 		bookingScheduler.setSmsSender(&testableSmsSender);
 		bookingScheduler.setMailSender(&testableMailSender);
@@ -111,10 +115,10 @@ TEST_F(BookingItem, 이메일이있는경우에는이메일발송) {
 }
 
 TEST_F(BookingItem, 현재날짜가일요일인경우예약불가예외처리) {
-	BookingScheduler* bookingScheduler = new SundayBookingScheduler{ CAPACITY_PER_HOUR };
+	BookingScheduler* bookingScheduler = new TestableBookingScheduler{ CAPACITY_PER_HOUR, SUNDAY_ON_THE_HOUR };
 
 	try {
-		Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL };
+		Schedule* schedule = new Schedule{ SUNDAY_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL };
 		bookingScheduler->addSchedule(schedule);
 		FAIL();
 	}
@@ -125,9 +129,9 @@ TEST_F(BookingItem, 현재날짜가일요일인경우예약불가예외처리) {
 }
 
 TEST_F(BookingItem, 현재날짜가일요일이아닌경우예약가능) {
-	BookingScheduler* bookingScheduler = new MondayBookingScheduler{ CAPACITY_PER_HOUR };
+	BookingScheduler* bookingScheduler = new TestableBookingScheduler{ CAPACITY_PER_HOUR, MONDAY_ON_THE_HOUR };
 
-	Schedule* schedule = new Schedule{ ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER_WITH_MAIL };
+	Schedule* schedule = new Schedule{ MONDAY_ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER_WITH_MAIL };
 	bookingScheduler->addSchedule(schedule);
 
 	EXPECT_EQ(true, bookingScheduler->hasSchedule(schedule));
